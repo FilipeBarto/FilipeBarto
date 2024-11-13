ceea6002bd8b8cd3abd95047f72faf61abd85524
filipe bartolomeu <fefezinho1996@gmail.com>
2024-11-13 10:10:49 -0300

    mensagem

---
+++ src/main/java/novo/Novo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package novo;
+
+/**
+ *
+ * @author filip
+ */
+public class Novo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
